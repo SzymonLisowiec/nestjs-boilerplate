3a405178d105f352d34c262805e89068b944227c
Kysune <szymonlisowiec@gmail.com>
2022-02-02 16:28:11 +0100

    Fixed set global prefix and loading env

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
-import * as Path from 'path';
-import * as DotEnv from 'dotenv';
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { Config } from './config';
-
-DotEnv.config({ path: Path.join(__dirname, '.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
-  app.setGlobalPrefix(Config.urlPrefix);
+  app.setGlobalPrefix(Config.prefix);
   await app.listen(Config.port, Config.host);
 }
 bootstrap();
